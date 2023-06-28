6ecf1a10db19b3c7744113347d689e8ff8d9739a
Pola Stróżak <pstrozak@edu.cdv.pl>
2023-06-28 19:44:35 +0200

    Cat & Lightning

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent nav;
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        nav.SetDestination(target.position);
+    }
+}
