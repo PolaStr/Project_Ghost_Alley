8d504dee5fe69ca349fd473887ff5f3d9217eef2
Pola Stróżak <pstrozak@edu.cdv.pl>
2023-06-28 20:05:43 +0200

    Quick save

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,9 +7,12 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
     NavMeshAgent nav;
+    public GameObject cat;
+
 
     private void Start()
     {
+        cat.transform.Rotate(90, 0, 0);
         nav = GetComponent<NavMeshAgent>();
     }
 
